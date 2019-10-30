52f672968b3c5d978a3f4fa29e98551126a709a1
Dan Johnson <danimal.johnson@gmail.com>
2019-10-30 02:16:00 -0700

    Fully functional with styling.

--- src/App.js
+++ src/App.js
@@ -5,41 +5,40 @@ import UserFriends from './components/UserFriends';
 // import { userInfo } from 'os';
 
 
-const GITHUB_USERNAME = 'danimal-johnson';
+const GITHUB_USERNAME = 'wvanorder';
 const API_BASE_URL = 'https://api.github.com/';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      username: GITHUB_USERNAME,
       userinfo: {}
     };
   }
 
   componentDidMount() {
     axios
-      .get(`${API_BASE_URL}users/${GITHUB_USERNAME}`)
+      .get(`${API_BASE_URL}users/${this.state.username}`)
       .then(res => {
-        console.log(res);
         this.setState({ userinfo: res.data })
       })
-      .catch(err => console.log(err));
+      .catch(err => console.error(err));
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Not using this function yet.
-    if (this.state.userinfo !== prevState.userinfo)
-      console.log("Something changed!");
-      console.log("We need",this.state.userinfo.followers_url)
+    // Not necessary
+    //if (this.state.userinfo !== prevState.userinfo)
+    //  console.log("Now available: ",this.state.userinfo.followers_url)
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>{`GitHub friends of ${GITHUB_USERNAME}`}</h1>
-        </header>
-        <p>Going to send this: {this.state.userinfo.followers_url}</p>
+        <header className="App-header"></header>
+        <h1>{`GitHub calendar and friends of ${this.state.userinfo.name}`}</h1>
+        <img src={`http://ghchart.rshah.org/${GITHUB_USERNAME}`}
+            alt="Github calendar graph"></img>
         <UserFriends friendsUrl={this.state.userinfo.followers_url} uncle="uncle" />
       </div>
     );
--- src/components/UserFriends.js
+++ src/components/UserFriends.js
@@ -1,30 +1,48 @@
 import React from 'react';
-// import axios from "axios"
+import axios from 'axios';
+import UserCard from './UserCard';
 
 class UserFriends extends React.Component {
   constructor() {
     super();
     this.state = {
-      peeps: []
+      peeps: [],
+      friendsUrl: ''
       // Do we need something here?
     };
   }
 
   componentDidMount(props) {
-    console.log("Props:", this.props);
+  // This function isn't needed
+  // console.log("Props:", this.props);
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Not using this function yet.
-    if (this.props.url !== prevProps.url) {
-      console.log("Yeah, I noticed it too.");
+    
+    // Store the new URL when it's ready.
+    if (this.props !== prevProps) {
+      this.setState({friendsUrl: this.props.friendsUrl});
     }
-  }
+
+    // User the new URL to get the list of friends
+    if (this.state !== prevState) {
+      if( this.state.friendsUrl !== '' && 
+          this.state.friendsUrl !== prevState.friendsUrl) {
+        axios
+        .get(this.state.friendsUrl)
+        .then(res => {
+          this.setState({ peeps: res.data })
+        })
+        .catch(err => console.error(err));
+      } // New URL received
+    } // New State
+  } // componentDidUpdate
 
   render() {
     return (
-      <div className="App">
-        <p>Whelp, we done got to the place where the friends will live.</p>
+      <div className="friends">
+        {this.state.peeps.map(friend => 
+          (<UserCard person={friend} key={friend.id} />))}
       </div>
     );
   }
